c3de051beb9dbb864867f8ce2f9a717ba656a4be
Angel A. Feliz <107366239+AngelAFeliz@users.noreply.github.com>
2022-08-05 13:58:44 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Estructuras-y-uniones/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+/* Estructuras-1. 
+El programa muestra la manera en que se declara una estructura, así como la➥forma en que se tiene acceso a los campos de las variables de tipo estructura➥tanto para asignación de valores como para lectura y escritura. */
+
+struct alumno          /* Declaración de la estructura. */
+{
+int matricula;
+char nombre[20];
+char carrera[20];          /* Campos de la estructura. */
+float promedio;
+char direccion[20];
+};     
+       /* Observa que la declaración de una estructura termina con punto y ➥coma. */
+
+
+
+int main(void)
+{
+ 
+return 0;
+}
+
+    
